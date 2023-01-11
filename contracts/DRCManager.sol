8c1eeba86f67150e620880d78dcc18bc28044ec1
Ras-Dwivedi <rasd.phd@gmail.com>
2023-01-11 13:26:28 +0530

    feat: DRC utilization application

--- contracts/DRCManager.sol
+++ contracts/DRCManager.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.16;
 import "./DRC.sol";
 import "./UserManager.sol";
 import "./Application.sol";
+import "./UtilizationApplication.sol";
 
 /**
 @title TDR Manager for TDR storage
@@ -14,6 +15,7 @@ contract DRCManager{
     DrcStorage public drcStorage;
     UserManager public userManager;
     DrcTransferApplicationStorage public dtaStorage;
+    DuaStorage public duaStorage;
     enum ApplicationStatus {pending, submitted, approved, rejected}
 
     // Address of the contract admin
@@ -119,99 +121,62 @@ contract DRCManager{
         drcStorage.updateDrc(drc.id,drc);
     }
 
-// // what other details, like building application are needed fro utilization application
-//  function createUtilizationApplication(bytes32 drcId,bytes32 applicationId, uint far) public {
-//         // check drc exists or not
-//         require(drcStorage.isDrcCreated(drcId),"DRC not created");
-//         DrcStorage.DRC memory drc = drcStorage.getDrc(drcId);
-//         // far should be less than available far.
-//         require(far <= drc.farAvailable, "Utilized area is greater than the available area");
-//         // add all the owners id from the drc to the mapping
-//         DrcTransferApplication storage application = applicationMap[applicationId];
-//         application.id = applicationId;
-//         application.drcId = drcId;
+// what other details, like building application are needed fro utilization application
+ function createUtilizationApplication(bytes32 drcId,bytes32 applicationId, uint far) public {
+        // check drc exists or not
+        require(drcStorage.isDrcCreated(drcId),"DRC not created");
+        DrcStorage.DRC memory drc = drcStorage.getDrc(drcId);
+        // far should be less than available far.
+        require(far <= drc.farAvailable, "Utilized area is greater than the available area");
+        // add all the owners id from the drc to the mapping
 
-// // This function could be removed. All it does is to mark approval for the msg.sender. 
-//         for(uint i=0; i<drc.owners.length; i++){
-//             Signatory memory s;
-//             s.userId = drc.owners[i].id;
-//             if(userManager.getId(msg.sender)==drc.owners[i].id){
-//                 s.hasUserSigned=true;
-//             } else {
-//                 s.hasUserSigned = false;
-//             }
-//             application.signatories.push(s);
-//         }
+       DuaStorage.Signatory[] memory duaSignatories = new DuaStorage.Signatory[](drc.owners.length);
 
-//         application.status = ApplicationStatus.pending;
-//         applicationMap[applicationId]=application;
-//         uint sNo = drc.subDrcs.length;
-//         DrcStorage.SubDrc memory subDrc;
-//         subDrc.sNo = sNo+1;
-//         subDrc.far = far;
-//         subDrc.status = DrcStorage.SubDrcStatus.locked_for_utilization;
-//         subDrc.linkedDrcId = applicationId;
-//         // drc.subDrcs.push(subDrc); // This statement is not available in memory. so lets create a new subDrc array, and then transfer it to Drc;
-//         DrcStorage.SubDrc[] memory newSubDrcs = new DrcStorage.SubDrc[](drc.subDrcs.length+1);
-//         for (uint i=0; i< drc.subDrcs.length; i++){
-//             newSubDrcs[i]=drc.subDrcs[i];
-//         }
-//         newSubDrcs[drc.subDrcs.length]=(subDrc);
-//         drc.subDrcs = newSubDrcs;
-//         drc.farAvailable = drc.farAvailable-far;
+        // no user has signed yet
+        for(uint i=0; i<drc.owners.length; i++){
+            DuaStorage.Signatory memory s;
+            s.userId = drc.owners[i].id;
+            s.hasUserSigned = false;
+            duaSignatories[i]=s;
+        }
 
 
-//         // see the caller of the copntract is one of the owner
-//         // save all the new owners
-//         // make sure that all the subsequent request have the same data
-//         //lock the DRC
-//         // change the signatories of the DRC
 
-//     }
+        duaStorage.createApplication(applicationId,drc.id,far,duaSignatories,DuaStorage.Status.pending);
+ 
+        drc.farAvailable = drc.farAvailable-far;
 
-//    function drcUtilizationApprove(bytes32 applicationId) public {
-//         DrcTransferApplication storage application = applicationMap[applicationId];
-//         // make sure the user has not signed the transfer
-//         for (uint i=0;i<application.signatories.length;i++){
-//             Signatory memory signatory = application.signatories[i];
-//             if(signatory.userId == userManager.getId(msg.sender)){
-//                 require(!signatory.hasUserSigned,"User have already signed the application");
-//                 signatory.hasUserSigned = true;
-//             }
-//         }
-//         // user signs the application
-//         // find out whether all the users have signed
-//         bool allSignatoriesSign = true;
-//         for (uint i=0;i<application.signatories.length;i++){
-//             Signatory memory s = application.signatories[i];
-//             if(!s.hasUserSigned){
-//                 allSignatoriesSign = false;
-//                 break;
-//             }
-//         }
-//         // if all the signatories has not signed
-//         if(allSignatoriesSign){
-//             //all the signatories has signed
-//             //change the status of the sub-drc
-//             application.status = ApplicationStatus.approved;
-//             applicationMap[applicationId]=application;
 
-//         // update DRC after updating subdrc
-//         DrcStorage.DRC memory drc = drcStorage.getDrc(application.drcId);
-//         DrcStorage.SubDrc memory subDrc;
-//         for (uint i=0;i<drc.subDrcs.length;i++){
-//             DrcStorage.SubDrc memory sd = drc.subDrcs[i];
-//             if(sd.linkedDrcId == applicationId){
-//                 sd.status = DrcStorage.SubDrcStatus.utilized;
-//                 subDrc = sd;
-//                 break;
-//             }
-//         }
-//         drc.subDrcs[subDrc.sNo-1] = subDrc;
-//         drcStorage.updateDrc(drc.id,drc);
-//         }
+    }
+
+   function drcUtilizationApprove(bytes32 applicationId) public {
+        DuaStorage.DUA  memory application = duaStorage.getApplication(applicationId);
+        // make sure the user has not signed the transfer
+        for (uint i=0;i<application.signatories.length;i++){
+            DuaStorage.Signatory memory signatory = application.signatories[i];
+            if(signatory.userId == userManager.getId(msg.sender)){
+                require(!signatory.hasUserSigned,"User have already signed the application");
+                signatory.hasUserSigned = true;
+            }
+        }
+        // user signs the application
+        // find out whether all the users have signed
+        bool allSignatoriesSign = true;
+        for (uint i=0;i<application.signatories.length;i++){
+            DuaStorage.Signatory memory s = application.signatories[i];
+            if(!s.hasUserSigned){
+                allSignatoriesSign = false;
+                break;
+            }
+        }
+        // if all the signatories has not signed
+        if(allSignatoriesSign){
+            //all the signatories has signed
+            application.status = DuaStorage.Status.approved;
+        }
+        duaStorage.updateApplication(application);
      
-//     }
+    }
 
 
 }
---
+++ contracts/UtilizationApplication.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+import "./DRC.sol";
+import "./UserManager.sol";
+
+contract DuaStorage {
+    enum Status {pending, submitted, approved, rejected}
+
+    struct DUA {
+        bytes32 id;
+        bytes32 drcId;
+        uint farUtilized;
+        Signatory[] signatories;
+        Status status;
+    }
+
+    struct Signatory {
+        bytes32 userId;
+        bool hasUserSigned;
+    }
+
+    mapping(bytes32 => DUA) public applicationMap;
+
+
+    address owner;
+    address admin;
+    address manager;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action.");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin || msg.sender == owner, "Only the admin or owner can perform this action.");
+        _;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only the manager, admin, or owner can perform this action.");
+        _;
+    }
+
+    function setAdmin(address _admin) public onlyOwner {
+        admin = _admin;
+    }
+
+    function setManager(address _manager) public {
+        require (msg.sender == owner ||  msg.sender == admin);
+        manager = _manager;
+    }
+
+    function createApplication(DUA memory dta) public onlyAdmin{
+        require(applicationMap[dta.id].id =="","application already exist");
+        storeApplicationInMap(dta);
+    }
+
+    function createApplication(bytes32 _id, bytes32 _drcId, uint _farTransferred, Signatory[] memory _signatories, Status _status) public onlyManager{
+        require(applicationMap[_id].id =="","application already exist");
+        storeApplicationInMap(DUA(_id, _drcId, _farTransferred, _signatories, _status));
+    }
+
+    function updateApplication(DUA memory dta) public onlyAdmin{
+        require(applicationMap[dta.id].id !="","application does not exist");
+        storeApplicationInMap(dta);
+
+    }
+
+    function updateApplication(bytes32 _id, bytes32 _drcId, uint _farTransferred, Signatory[] memory _signatories, Status _status) public onlyManager{
+        require(applicationMap[_id].id !="","application does not exist");
+        storeApplicationInMap(DUA(_id, _drcId, _farTransferred, _signatories, _status));
+    }
+
+    function getApplication(bytes32 _id) view public returns(DUA memory){
+       require(applicationMap[_id].id !="","application does not exist");
+       return applicationMap[_id];
+    }
+
+    function deleteApplication(bytes32 _id) public onlyManager {
+        require(msg.sender == admin, "Only the admin can delete applications.");
+        delete applicationMap[_id];
+    }
+    // This function just creates a new appliction in the mapping based on the applicaiton in the memory
+    function storeApplicationInMap (DUA memory _dta) internal {
+        DUA storage dta = applicationMap[_dta.id];
+        
+        dta.id = _dta.id;
+        dta.drcId = _dta.drcId;
+        dta.farUtilized = _dta.farUtilized;
+        dta.status = _dta.status;
+        for (uint i =0; i< _dta.signatories.length; i++){
+            dta.signatories[i]=_dta.signatories[i];
+        }
+
+
+        applicationMap[dta.id]=dta;
+    }
+}
