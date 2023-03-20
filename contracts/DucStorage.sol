9e5e5a0aafe780a4ae3f588a646f2d5baf76daf3
Ras-Dwivedi <rasd.phd@gmail.com>
2023-03-20 15:51:50 +0530

    add fetch duc

--- contracts/DucStorage.sol
+++ contracts/DucStorage.sol
@@ -10,48 +10,61 @@ import "./DataTypes.sol";
 
 // Define the struct that we will be using for our CRUD operations
 
-contract DUCStorage {
+contract DucStorage {
     mapping(bytes32 => DUC) private certMap;
     mapping(bytes32 => bytes32[]) public ownerMap; //ownerId => DucId
     mapping(bytes32 => bytes32[]) public applicationMap; //applicationId => DucId[]
     // An application can have multiple DUC
 
-    // Create a function to add a new Drc to the mapping
-    function createDrc(DRC memory _drc) public onlyDrcCreator{
-        //check whether the DRC already exists
-        require(!isDrcCreated(_drc.id),"DRC already exists");
-        addDrcToOwners(_drc);
-        storeDrcInMap(_drc);
-        emit DrcCreated(_drc.id, _drc.owners);
+    event Logger(string log);
+    event LogAddress(string addressInfo, address _address);
+    event LogBytes(string messgaeInfo, bytes32 _bytes);
+    event LogBool(string messageInfo, bool message);
+    event LogApplication(string message, TdrApplication application);
+
+    address owner;
+    address admin;
+    address manager;
+    address tdrManager;
+
+    // Constructor function to set the initial values of the contract
+    constructor(address _admin, address _manager) {
+        // Set the contract owner to the caller
+        owner = msg.sender;
+
+        // Set the contract admin
+        admin = _admin;
+        manager = _manager;
     }
-    // Create a function to update a Drc in the mapping
-    function updateDrc(bytes32 _id, DRC memory _drc) public onlyManager {
-        // the drc should exist
-        // Update the Drc in the mapping
-        require(_id ==_drc.id, "drcid should be same");
-        require(isDrcCreated(_drc.id),"DRC does not exists");
-        // insertDrc((_drc));
-        storeDrcInMap(_drc);
+    // Modifier to check if the caller is the TDR manager
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the TDR manager");
+        _;
     }
 
-    // Create a function to retrieve a Drc from the mapping by ID
-    function getDrc(bytes32 _id) public view returns (DRC memory) {
-        // Retrieve the Drc from the mapping
-        DRC memory drc = drcMap[_id];
+    // Modifier to check if the caller is the contract admin
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Caller is not the contract admin");
+        _;
+    }
+    modifier onlyManager() {
+        require(msg.sender == manager, "Caller is not the contract manager");
+        _;
+    }
 
-        // Return the Drc's ID, name, and age
-        return drc;
+    function setAdmin(address _newAdmin) onlyOwner public{
+        admin = _newAdmin;
+    }
+    function setOwner(address _newOwner) onlyOwner public {
+        owner = _newOwner;
+    }
+    function setManager(address _newManager) onlyOwner public {
+        manager = _newManager;
     }
 
 
 
-    // Create a function to delete a Drc from the mapping
-    function deleteDrc(bytes32 _id) public onlyOwner{
-        // Delete the Drc from the mapping
-        delete drcMap[_id];
-    }
-//############################333
-    function createCert(
+    function createDuc(
         bytes32 id,
         bytes32 applicationId,
         bytes32 noticeId,
@@ -61,7 +74,7 @@ contract DUCStorage {
         bytes32[] memory owners,
         uint timeStamp
     ) public {
-        certMap[id] = DrcUtilizationCert({
+        certMap[id] = DUC({
         id: id,
         applicationId: applicationId,
         noticeId: noticeId,
@@ -75,26 +88,26 @@ contract DUCStorage {
     event DucCreated(bytes32 ducId, bytes32[] ownerIds);
     event DucUpdated(bytes32 ducId, bytes32[] ownerIds);
 
-    function createDrc(DUC memory _duc) public onlyDrcCreator{
+    function createDuc(DUC memory _duc) public onlyManager{
         //check whether the DRC already exists
         require(!isDucCreated(_duc.id),"certificate already exists");
-        addDrcToOwners(_drc);
-        storeDucInMap(_drc);
-        emit DucCreated(_drc.id, _drc.owners);
+        addDucToOwners(_duc);
+        storeDucInMap(_duc);
+        emit DucCreated(_duc.id, _duc.owners);
     }
-    function updateDuc(DUC memory _duc) public onlyDrcCreator{
+    function updateDuc(DUC memory _duc) public onlyManager{
         //check whether the DRC already exists
         require(isDucCreated(_duc.id),"certificate does not exists");
-        addDrcToOwners(_drc);
-        storeDucInMap(_drc);
-        emit DucUpdated(_drc.id, _drc.owners);
+        addDucToOwners(_duc);
+        storeDucInMap(_duc);
+        emit DucUpdated(_duc.id, _duc.owners);
     }
 
-    function readCert(bytes32 id) public view returns (DUC memory) {
+    function getDuc(bytes32 id) public view returns (DUC memory) {
         return certMap[id];
     }
 
-    function updateCert(
+    function updateDuc(
         bytes32 id,
         bytes32 applicationId,
         bytes32 noticeId,
@@ -105,7 +118,7 @@ contract DUCStorage {
         uint timeStamp
     ) public {
         require(certMap[id].id == id, "Cert does not exist");
-        certMap[id] = DrcUtilizationCert({
+        certMap[id] = DUC({
         id: id,
         applicationId: applicationId,
         noticeId: noticeId,
@@ -117,13 +130,13 @@ contract DUCStorage {
         });
     }
 
-    function deleteCert(bytes32 id) public {
+    function deleteDuc(bytes32 id) public {
         require(certMap[id].id == id, "Cert does not exist");
         delete certMap[id];
     }
 
-    function isDucCreated(bytes32 ducId) public {
-        if(certMap[_ducId].id !=""){
+    function isDucCreated(bytes32 ducId) public returns (bool) {
+        if(certMap[ducId].id !=""){
             return true;
         }
         return false;
@@ -132,14 +145,13 @@ contract DUCStorage {
 
     function addDucToOwners(DUC memory duc) public {
         for (uint i=0; i< duc.owners.length; i++){
-            addDrcToOwner(duc.id,duc.owners[i]);
+            addDucToOwner(duc.id,duc.owners[i]);
         }
     }
     function addDucToOwner(bytes32 ducId, bytes32 ownerId) public {
         bytes32[] storage ducList = ownerMap[ownerId];
         ducList.push(ducId);
         ownerMap[ownerId]=ducList;
-        emit DrcAddedToOwner(ducId,ownerId);
     }
     function storeDucInMap (DUC memory _duc) internal {
         //        drcMap[_drc.id]=_drc;
@@ -171,7 +183,7 @@ contract DUCStorage {
             revert("owner already exist, try updating list");
         }
         ownerMap[ownerId] = ducList;
-        emit DrcIdsAdded(ownerId, ducList);
+        emit DucIdsAdded(ownerId, ducList);
     }
 
     event DucIdsUpdated(bytes32 ownerId, bytes32[] drcList);
@@ -202,7 +214,6 @@ contract DUCStorage {
         return ownerMap[userId];
     }
 
-    //################333
     function addDucIdsToApplication(bytes32[] memory ducList, bytes32 applicationId) public onlyManager{
         if(isApplicationInApplicationMap(applicationId)){
             revert("owner already exist, try updating list");
@@ -233,6 +244,11 @@ contract DUCStorage {
             return false;
         }
         return true;
+    }
+    function addDucToApplication(bytes32 ducId,bytes32 applicationId) public {
+        bytes32[] storage ducList = applicationMap[applicationId];
+        ducList.push(applicationId);
+        applicationMap[applicationId]=ducList;
     }
 
 }
