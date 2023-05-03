bd5c5e90cd0dedef57a2abed92056f7d493be428
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2023-05-03 12:08:34 +0530

    Adding onlymanager to/and removing of duplicate update  contract_address

--- contracts/UserManager.sol
+++ contracts/UserManager.sol
@@ -19,7 +19,7 @@ contract UserManager is KdaCommon {
     UserStorage public userStorage;
     address public userStorageAddress;
 
-    function loadUserStorage(address _userStorageAddress) public {
+    function loadUserStorage(address _userStorageAddress) public onlyManager{
         userStorageAddress = _userStorageAddress;
         userStorage = UserStorage(userStorageAddress);
     }
